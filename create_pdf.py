9a5be30c99b437f26ab8e0faa4f1bd6fb5104f3b
asifnawazofficial <53863047+asifnawazofficial@users.noreply.github.com>
2022-06-26 22:22:20 +0500

    5 quotes from a list outputted on a pdf

--- create_pdf.py
+++ create_pdf.py
@@ -1,10 +1,13 @@
 from fpdf import FPDF
 
+quotes = ['This is quote #1', 'This is quote #2', 'This is quote #3', 'This is quote #4', 'This is quote #5']
+
 pdf = FPDF('P', 'mm', 'Letter')
 pdf.add_page()
 
 pdf.set_font('helvetica', '', 16)
 
-pdf.cell(40,10,'Hello World! This is working.')
+for quote in quotes:
+	pdf.cell(40,10, quote, ln=True)
 
 pdf.output('output.pdf')
